a62bf9567e900bb2c22257ae386eb26ed254d590
Andrey Skryl <ya.skryl@yandex.ru>
2016-07-17 11:07:18 +0600

    После некоторого refactoring-а.

--- initial/frontend/app/components/toolbar/toolbar.dir.js
+++ initial/frontend/app/components/toolbar/toolbar.dir.js
@@ -20,7 +20,42 @@
 			vm.logout = logout;
 			vm.auth = auth;
 
-			function login() {
+			function login (email, password, callback) {
+				var connection = mysql({
+					host     : 'localhost',
+					user     : 'root',
+					password : 'password',
+					database : 'todo_list'
+				});
+
+				connection.connect();
+
+				var query = "SELECT GUID, LOGIN, EMAIL, PASSWORD, LASTNAME, FIRSTNAME " +
+					"FROM user WHERE EMAIL = ?";
+
+				connection.query(query, [email], function (err, results) {
+					if (err) return callback(err);
+					if (results.length === 0) return callback();
+					var user = results[0];
+
+					if (!bcrypt.compareSync(password, user.password)) {
+						return callback();
+					}
+
+					callback(null,   {
+						guid:      user.guid.toString(),
+						login:     user.login.toString(),
+						eMail:     user.eMail.toString(),
+						password:  user.password.toString(),
+						lastName:  user.lastName.toString(),
+						firstName: user.firstName.toString()
+					});
+
+				});
+
+			}
+
+			/*function login() {
 				auth.signin({}, function (profile, token) {
 					store.set('profile', profile);
 					store.set('id_token', token);
@@ -35,7 +70,7 @@
 				store.remove('id_token');
 				auth.signout();
 				$location.path('/');
-			}
+			}*/
 
 		}]);
 })();
